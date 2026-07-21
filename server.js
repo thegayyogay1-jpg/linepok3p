c51051192cbfd8c9822f0e2f65d97925b1e1b50b
thegayyogay1-jpg <thegayyogay1@gmail.com>
2026-07-21 18:12:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1403,7 +1403,7 @@ else if (userMsg === 'oo' || userMsg === 'xx') {
                         let betTracker = usersRoundCrossCheck[userId];
 
                         const allowedLegs = ['1', '2', '3', '4'];
-                        const MIN_BET = 20;
+                        const MIN_BET = 10;
                         const MAX_BET = 2500;
 
                         for (let line of lines) {

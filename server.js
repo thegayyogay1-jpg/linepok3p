21d9cb67db01ee4b178a814fa1efca57e6ca2f4c
thegayyogay1-jpg <thegayyogay1@gmail.com>
2026-08-23 10:21:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2762,7 +2762,7 @@ else if (userMsg === 'ok' || userMsg === 'no') {
                 summaryPayoutText += `👑 เจ้ามือ: ${tempDealerResult.name}\n`;
             
                 const hiloSum = tempHiloDices.reduce((a, b) => a + b, 0);
-                summaryPayoutText += `🎲 ผลไฮโล: [ ${tempHiloDices.join("-")} ] (${hiloSum} แต้ม)\n`;
+                summaryPayoutText += `🎲 ผลไฮโล: [ ${hasHiloResult.join("-")} ] (${hiloSum} แต้ม)\n`;
             
             summaryPayoutText += `──────────────────\n`;
             
@@ -3139,7 +3139,7 @@ else if (userMsg === 'ok' || userMsg === 'no') {
                             { "type": "text", "text": "💰 สรุปยอดได้/เสีย ประจำรอบ 🎉", "weight": "bold", "color": "#ffaa00", "size": "md", "align": "center" },
                             { "type": "text", "text": `รอบที่: ${currentRound} (หน้า ${index + 1}/${userPages.length})`, "weight": "bold", "color": "#ffffff", "size": "xl", "align": "center", "margin": "none" },
                             { "type": "text", "text": `👑 เจ้ามือป๊อกเด้ง: ${tempDealerResult.name}`, "size": "xs", "color": "#aaaaaa", "align": "center" }
-                            { "type": "text", "text": `🎲 ลูกเต๋าไฮโล: ${tempHiloDices.join(" - ")} (${tempHiloDices.reduce((a,b)=>a+b,0)} แต้ม)`, "size": "xs", "color": "#ffcc00", "align": "center" }
+                            { "type": "text", "text": `🎲 ลูกเต๋าไฮโล: ${hasHiloResult.join("-")} (${tempHiloDices.reduce((a,b)=>a+b,0)} แต้ม)`, "size": "xs", "color": "#ffcc00", "align": "center" }
                             { "type": "separator", "color": "#2a2a35" },
                             
                             // 👤 รายชื่อสมาชิก
